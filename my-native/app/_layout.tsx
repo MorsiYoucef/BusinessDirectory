a8749cd6e083930206288a5dfe2bcd25f96b4745
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-06-27 18:01:14 +0100

    new commit

--- my-native/app/_layout.tsx
+++ my-native/app/_layout.tsx
@@ -1,9 +1,28 @@
 import { Stack } from 'expo-router'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo'
+import { Text } from 'react-native'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  )
+}
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey}>
+      <SignedIn>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <Text>Sign-out</Text>
+      </SignedOut>
+    </ClerkProvider>
+
+    //
   )
 }
