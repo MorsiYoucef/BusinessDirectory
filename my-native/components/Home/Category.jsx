c8ecd7e07b1d7caca8fbdb5fa91492a7d8262c18
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-06-29 19:34:19 +0100

    new commit

--- my-native/components/Home/Category.jsx
+++ my-native/components/Home/Category.jsx
@@ -1,8 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { Colors } from '../../constants/Colors'
+import CategoryItem from './CategoryItem'
 
 const Category = () => {
+  const [categoryList, setCategoryList] = useState([])
+
+  useEffect(() => {
+    GetCategoryList()
+  }, [])
+  const GetCategoryList = async () => {
+    const q = query(collection(db, 'Category'))
+    const querySnaphot = await getDocs(q)
+
+    querySnaphot.forEach((doc) => {
+      console.log(doc.data())
+      setCategoryList((prev) => [...prev, doc.data()])
+    })
+  }
   return (
     <View>
       <View
@@ -27,12 +42,16 @@ const Category = () => {
           style={{
             color: Colors.PRIMARY,
             paddingRight: 15,
-            fontFamily: 'Outfit-Med',
+            fontFamily: 'Outfit-Med ',
           }}
         >
           View All
         </Text>
       </View>
+      <FlatList
+        data={categoryList}
+        renderItem={({ item, index }) => <CategoryItem Category={item} />}
+      />
     </View>
   )
 }
