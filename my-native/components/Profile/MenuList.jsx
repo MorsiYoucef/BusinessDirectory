f38c72244eb4f61d94883690021c049ac7440393
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-07-02 18:58:35 +0100

    new commit

--- my-native/components/Profile/MenuList.jsx
+++ my-native/components/Profile/MenuList.jsx
@@ -1,10 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Colors } from './../../constants/Colors'
 
 const MenuList = () => {
+  const menuList = [
+    {
+      id: 1,
+      name: 'Add Business',
+      icon: require('./../../assets/images/add.png'),
+      path: '',
+    },
+    {
+      id: 2,
+      name: 'My Business',
+      icon: require('./../../assets/images/business-and-trade.png'),
+      path: '',
+    },
+    {
+      id: 3,
+      name: 'Share App',
+      icon: require('./../../assets/images/share_1.png'),
+      path: '',
+    },
+    {
+      id: 4,
+      name: 'Logout',
+      icon: require('./../../assets/images/logout.png'),
+      path: '',
+    },
+  ]
   return (
-    <View>
-      <Text>MenuList</Text>
+    <View style={{ marginTop: 50 }}>
+      <FlatList
+        data={menuList}
+        numColumns={2}
+        renderItem={({ item, index }) => (
+          <View
+            key={index}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 5,
+              flex: 1,
+              margin: 10,
+              padding: 10,
+              borderWidth: 1,
+              borderRadius: 20,
+              backgroundColor: '#fff',
+            }}
+          >
+            <Image source={item.icon} style={{ width: 60, height: 60 }} />
+            <Text style={{ fontFamily: 'Outfit-Med', fontSize: 15, flex: 1 }}>
+              {' '}
+              {item.name}
+            </Text>
+          </View>
+        )}
+      />
+      <Text
+        style={{
+          fontFamily: 'Outfit',
+          textAlign: 'center',
+          marginTop: 100,
+          color: Colors.GRAY,
+        }}
+      >
+        Developed by Yusuf @2024
+      </Text>
     </View>
   )
 }
